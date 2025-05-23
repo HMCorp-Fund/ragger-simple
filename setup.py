92d8c60185de3f48419b89d74a6bd6b828b51bc4
Anton Pavlenko <apavlenko@hmcorp.fund>
2025-05-23 20:05:28 +0500

    Bump version to 0.1.3 in __init__.py and setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ragger-simple",
-    version="0.1.2",
+    version="0.1.3",
     author="Anton Pavlenko",
     author_email="apavlenko@hmcorp.fund",
     description="Simple vector database operations with Qdrant",
